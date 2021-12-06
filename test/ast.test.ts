6312fa258084c0a372010f10280e805d5fe76732
Daniel Roe <daniel@roe.dev>
2021-12-06 14:18:52 +0100

    chore: add mocha test suite (#1)

---
+++ test/ast.test.ts
@@ -0,0 +1,27 @@
+import { expect } from 'chai'
+import { parse, proxifyAST, generate } from '../src'
+
+describe('proxyAST', () => {
+  it('should produce correct code', () => {
+    const ast = proxifyAST(
+      parse(`
+      export const a = {}
+      export default {
+        // This is foo
+        foo: ['a']
+      }`)
+    )
+
+    ast.exports.default.props.foo.push('b')
+
+    // eslint-disable-next-line no-console
+    expect(generate(ast).code).to.equal(
+        `
+      export const a = {}
+      export default {
+        // This is foo
+        foo: ['a', "b"]
+      }`
+    )
+  })
+})
