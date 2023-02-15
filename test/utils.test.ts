c58699b4bafb1a84f127732ad5f032fa278d1657
Anthony Fu <anthonyfu117@hotmail.com>
2023-02-15 14:58:46 +0100

    chore: lint

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -2,22 +2,22 @@ import { describe, it, expect } from "vitest";
 import { print } from "recast";
 import { literalToAst } from "../src/proxy/_utils";
 
-describe('literalToAst', () => {
+describe("literalToAst", () => {
   // eslint-disable-next-line unicorn/consistent-function-scoping
   function run(value: any) {
     return print(literalToAst(value)).code;
   }
-  
-  it('should work', () => {
-    expect(run(1)).toMatchInlineSnapshot('"1"')
-    expect(run(true)).toMatchInlineSnapshot('"true"')
-    expect(run(undefined)).toMatchInlineSnapshot('"undefined"')
+
+  it("should work", () => {
+    expect(run(1)).toMatchInlineSnapshot('"1"');
+    expect(run(true)).toMatchInlineSnapshot('"true"');
+    expect(run(undefined)).toMatchInlineSnapshot('"undefined"');
     // eslint-disable-next-line unicorn/no-null
-    expect(run(null)).toMatchInlineSnapshot('"null"')
-    expect(run([undefined, 1, { foo:'bar' }])).toMatchInlineSnapshot(`
+    expect(run(null)).toMatchInlineSnapshot('"null"');
+    expect(run([undefined, 1, { foo: "bar" }])).toMatchInlineSnapshot(`
       "[undefined, 1, {
           foo: \\"bar\\"
       }]"
-    `)
-  })
-})
+    `);
+  });
+});
