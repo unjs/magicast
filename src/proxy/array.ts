85aa0f15b02e53db7f15b7c04f858d5cb24f1a88
userquin <userquin@gmail.com>
2025-10-23 12:41:52 +0200

    chore: emulate common non-mutating iterator methods on proxified arrays

--- src/proxy/array.ts
+++ src/proxy/array.ts
@@ -6,10 +6,11 @@ import { proxify } from "./proxify";
 export function proxifyArrayElements<T extends any[]>(
   node: ASTNode,
   elements: ASTNode[],
-  mod?: ProxifiedModule,
+  mod?: ProxifiedModule
 ): ProxifiedArray<T> {
   const utils = makeProxyUtils(node, {
     $type: "array",
+    // Mutator methods - they modify the underlying AST
     push(value: any) {
       elements.push(literalToAst(value) as any);
     },
@@ -26,20 +27,9 @@ export function proxifyArrayElements<T extends any[]>(
       const deleted = elements.splice(
         start,
         deleteCount,
-        ...items.map((n) => literalToAst(n)),
+        ...items.map((n) => literalToAst(n))
       );
       return deleted.map((n) => proxify(n as any, mod));
-    },
-    find(predicate: (value: any, index: number, arr: any[]) => boolean) {
-      // eslint-disable-next-line unicorn/no-array-callback-reference
-      return elements.map((n) => proxify(n as any, mod)).find(predicate);
-    },
-    findIndex(predicate: (value: any, index: number, arr: any[]) => boolean) {
-      // eslint-disable-next-line unicorn/no-array-callback-reference
-      return elements.map((n) => proxify(n as any, mod)).findIndex(predicate);
-    },
-    includes(value: any) {
-      return elements.map((n) => proxify(n as any, mod)).includes(value);
     },
     toJSON() {
       return elements.map((n) => proxify(n as any, mod));
@@ -51,6 +41,96 @@ export function proxifyArrayElements<T extends any[]>(
       if (key in utils) {
         return (utils as any)[key];
       }
+
+      // Emulated non-mutating methods - they operate on the proxified elements
+      const self = receiver as any[];
+      if (key === 'map') {
+        return (callback: (value: any, index: number, array: any[]) => any) => {
+          const results = [];
+          let index = 0;
+          for (const item of self) {
+            results.push(callback(item, index, self));
+            index++;
+          }
+          return results;
+        };
+      }
+      if (key === 'filter') {
+        return (callback: (value: any, index: number, array: any[]) => boolean) => {
+          const results = [];
+          let index = 0;
+          for (const item of self) {
+            if (callback(item, index, self)) {
+              results.push(item);
+            }
+            index++;
+          }
+          return results;
+        };
+      }
+      if (key === 'forEach') {
+        return (callback: (value: any, index: number, array: any[]) => void) => {
+          let index = 0;
+          for (const item of self) {
+            callback(item, index, self);
+            index++;
+          }
+        };
+      }
+      if (key === 'reduce') {
+        return (callback: (previousValue: any, currentValue: any, currentIndex: number, array: any[]) => any, ...initialValue: [any?]) => {
+          const array = [...self];
+          if (array.length === 0 && initialValue.length === 0) {
+            throw new TypeError("Reduce of empty array with no initial value");
+          }
+
+          let accumulator: any;
+          let startIndex = 0;
+
+          if (initialValue.length > 0) {
+            accumulator = initialValue[0];
+          } else {
+            accumulator = array[0];
+            startIndex = 1;
+          }
+
+          for (let i = startIndex; i < array.length; i++) {
+            accumulator = callback(accumulator, array[i], i, array);
+          }
+
+          return accumulator;
+        };
+      }
+      if (key === 'find') {
+        return (callback: (value: any, index: number, obj: any[]) => boolean) => {
+          let index = 0;
+          for (const item of self) {
+            if (callback(item, index, self)) {
+              return item;
+            }
+            index++;
+          }
+        };
+      }
+      if (key === 'findIndex') {
+        return (callback: (value: any, index: number, obj: any[]) => boolean) => {
+          let index = 0;
+          for (const item of self) {
+            if (callback(item, index, self)) {
+              return index;
+            }
+            index++;
+          }
+          return -1;
+        };
+      }
+      if (key === 'includes') {
+        return (searchElement: any, fromIndex?: number) => {
+            return [...self].includes(searchElement, fromIndex);
+        }
+      }
+
+      // Property access
       if (key === "length") {
         return elements.length;
       }
@@ -100,11 +180,7 @@ export function proxifyArrayElements<T extends any[]>(
     },
     getOwnPropertyDescriptor(target, key) {
       if (key in utils) {
-        return {
-          configurable: true,
-          enumerable: true,
-          value: (utils as any)[key],
-        };
+        return { configurable: true, enumerable: true, value: (utils as any)[key] };
       }
 
       if (key === "length") {
@@ -137,7 +213,7 @@ export function proxifyArrayElements<T extends any[]>(
 
 export function proxifyArray<T extends any[]>(
   node: ASTNode,
-  mod?: ProxifiedModule,
+  mod?: ProxifiedModule
 ): ProxifiedArray<T> {
   if (!("elements" in node)) {
     return undefined as any;
--- test/array.test.ts
+++ test/array.test.ts
@@ -57,4 +57,41 @@ describe("array", () => {
       ]
     `);
   });
+
+  it("array iterator methods", () => {
+    const mod = parseModule<{ default: { id: number }[] }>(
+      `export default [{ id: 1 }, { id: 2 }, { id: 3 }]`,
+    );
+
+    const arr = mod.exports.default;
+    expect(Array.isArray(arr)).toBe(true);
+
+    // .map()
+    const ids = arr.map((item) => item.id);
+    expect(ids).toEqual([1, 2, 3]);
+
+    // .filter()
+    const filtered = arr.filter((item) => item.id > 1);
+    expect(filtered.map((item) => item.id)).toEqual([2, 3]);
+    expect(filtered.length).toBe(2);
+
+    // .reduce()
+    const sum = arr.reduce((acc, item) => acc + item.id, 0);
+    expect(sum).toBe(6);
+
+    // .forEach()
+    let count = 0;
+    // eslint-disable-next-line unicorn/no-array-for-each
+    arr.forEach(() => count++);
+    expect(count).toBe(3);
+
+    // Destructuring (tests Symbol.iterator)
+    const [first] = arr;
+    expect(first.id).toBe(1);
+
+    // Spread syntax (tests Symbol.iterator)
+    const fullArray = [...arr];
+    expect(fullArray.length).toBe(3);
+    expect(fullArray[2].id).toBe(3);
+  });
 });
